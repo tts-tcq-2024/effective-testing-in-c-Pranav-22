3d680202810c0b8ab38995d59a1a6d65105f7de8
Pranav-22 <57517040+Pranav-22@users.noreply.github.com>
2024-08-09 16:39:52 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <assert.h>
 
+int printcount = 0;
+char messagebuffer[1024] = {0};
+
+void testprint(const char *format, ...) {
+    va_list args;
+    va_start(args, format);
+    strcpy();
+}
+
+
 int printColorMap() {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
