b9284caa8e8aaf074a81e4d18006d38ad1e41323
Pranav-22 <57517040+Pranav-22@users.noreply.github.com>
2024-08-15 13:07:53 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -1,18 +1,32 @@
 #include <stdio.h>
+#include<string.h>
+#include <stdarg.h>
 #include <assert.h>
 
 int alertFailureCount = 0;
+int printcount = 0;
+char testbuffer[1024] = {0};
+float convertedvalue = 0;
+
+void print(const char *format, ...)
+{
+    va_list args;
+    va_start(args, format);
+    vsprintf(testbuffer, format, args);
+    va_end(args);
+    printcount++;
+}
+
 
 int check_celcius_range(float celcius)
 {
-    if(celcius >= 0.0)
-        return 200;
-    else
-        return 500;
+    convertedvalue = celcius;
+    /*returning failure for network alert stub*/
+    return 500;
 }
 
 int networkAlertStub(float celcius) {
-    printf("ALERT: Temperature is %.1f celcius.\n", celcius);
+    print("ALERT: Temperature is %.1f celcius.\n", celcius);
     return check_celcius_range(celcius);
 }
 
@@ -24,10 +38,36 @@ void alertInCelcius(float farenheit) {
     }
 }
 
-int main() {
+void printtest(void)
+{
     alertInCelcius(400.5);
+    assert(!("ALERT: Temperature is 204.7 celcius." == testbuffer));
     alertInCelcius(303.6);
+    assert(!("ALERT: Temperature is 150.9 celcius." == testbuffer));
     alertInCelcius(10);
+    assert(!("ALERT: Temperature is -12.2 celcius." == testbuffer));
+}
+
+void alerttest(void)
+{
     assert(alertFailureCount == 1);
+}
+
+void conversiontest(void)
+{
+    alertInCelcius(400.5);
+    assert(convertedvalue == 204.7);
+
+    alertInCelcius(303.6);
+    assert(convertedvalue == 150.9);
+
+}
+
+
+int main()
+ {
+    printtest();
+    alerttest();
+    conversiontest();
     return 0;
 }
